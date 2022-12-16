4a674fc545a12df93488bdd081c3558a047f0903
Diokou Tech <diokoucheikh@gmail.com>
2022-12-16 17:25:55 +0100

    update first commit

---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Models
+{ 
+class Person
+{
+    public int taille { get; set; }
+    public int poids { get; set; }
+    public string nom { get; init; }
+    // init veut dire que la propriété est juste initialisable avec une valeur donnée et non modifiable tout au long du programme.
+
+    public override string ToString()
+    {
+        return $" your .taille is {taille} , your weigth is {poids} and your name is {nom}";
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static bool finish = false;
+        public static List<Person> Liste = new List<Person>();
+        static void Main(string[] args)
+        {
+            do{
+            Console.WriteLine("Donner votre nom complet !");
+            string nomComplet = Console.ReadLine();
+            Console.WriteLine("Donner votre taille !");
+            string taille = Console.ReadLine();
+            Console.WriteLine("Donner votre poids !");
+            string poids = Console.ReadLine();
+            var per = new Person {taille = Int32.Parse(taille), poids = Int32.Parse(poids), nom = nomComplet};
+            Liste.Add(per);
+            show(per.ToString());
+            pause();
+            }while(!finish);
+        }
+        public static void show(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        static void iterer()
+        {
+            short i =0;
+            while(i < 9 ){
+                show($"{i} est inférieur à 9");
+                i++;
+            }
+        }
+        public static void showListe(){
+            foreach (var item in Liste)
+            {
+                show(item.ToString());
+            }
+        }
+        static void pause()
+        {
+            Console.WriteLine("Appuyer sur 1 pour continuer !");
+            Console.WriteLine("Appuyer sur 0 pour quitter !");
+            string ligne = Console.ReadLine();
+            switch(Int32.Parse(ligne)){
+                case 1:  finish = false; break;
+                case 0: {
+                finish = true;show("Au revoir"); break;
+                } 
+            }
+        }
+    }
+}
